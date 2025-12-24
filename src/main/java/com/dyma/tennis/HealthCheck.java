248151e1df0198d883838551bff2940e9c87da56
Joel KOUEHI <kouehijoel05@gmail.com>
2025-12-24 12:56:58 +0100

    Firts commit - Spring boot Tennis Project

---
+++ src/main/java/com/dyma/tennis/HealthCheck.java
@@ -0,0 +1,5 @@
+package com.dyma.tennis;
+
+public record HealthCheck(ApplicationStatus status, String message) {
+    
+}
---
+++ src/main/java/com/dyma/tennis/rest/HealthCheckController.java
@@ -0,0 +1,20 @@
+package com.dyma.tennis.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dyma.tennis.HealthCheck;
+import com.dyma.tennis.service.HealthCheckService;
+
+@RestController
+public class HealthCheckController {
+
+    @Autowired
+    private HealthCheckService healthCheckService;
+
+    @GetMapping("/healthcheck")
+    public HealthCheck healthCheck() {
+        return healthCheckService.healthCheck();
+    }
+}
