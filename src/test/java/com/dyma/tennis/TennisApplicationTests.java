e6591a7dfb57f03fb0fe4c4f1a238ebe9469c941
Joel KOUEHI <kouehijoel05@gmail.com>
2025-12-25 20:49:37 +0100

    Add unit tests for HealthCheckController, HealthCheckService, and HealthCheckRepository

--- src/test/java/com/dyma/tennis/TennisApplicationTests.java
+++ src/test/java/com/dyma/tennis/TennisApplicationTests.java
@@ -1,13 +1,31 @@
 package com.dyma.tennis;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.dyma.tennis.repository.HealthCheckRepository;
+import com.dyma.tennis.service.HealthCheckService;
+import com.dyma.tennis.web.HealthCheckController;
 
 @SpringBootTest
 class TennisApplicationTests {
 
+	@Autowired
+	private HealthCheckController healthCheckController;
+
+	@Autowired
+	private HealthCheckService healthCheckService;
+
+	@Autowired
+	private HealthCheckRepository healthCheckRepository;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(healthCheckController).isNotNull();
+		Assertions.assertThat(healthCheckService).isNotNull();
+		Assertions.assertThat(healthCheckRepository).isNotNull();
 	}
 
 }
